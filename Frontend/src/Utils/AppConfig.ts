a13986c1c068a0951ac23d296737fb4064e7a2f7
Lior Bas <look551@gmail.com>
2024-12-03 18:59:28 +0200

    added function for incomplete button

--- Frontend/src/Utils/AppConfig.ts
+++ Frontend/src/Utils/AppConfig.ts
@@ -7,6 +7,7 @@ class AppConfig {
   public readonly addTask = "http://localhost:4000/api/tasks/";
   public readonly UpdateTask = "http://localhost:4000/api/tasks/";
   public readonly UpdateTaskToComplete = "http://localhost:4000/api/tasks/";
+  public readonly UpdateTaskToIncomplete = "http://localhost:4000/api/tasks/";
   public readonly deleteTask = "http://localhost:4000/api/tasks/";
 }
 
